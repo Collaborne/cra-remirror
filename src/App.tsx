755b13e8ac1a6fa0724a6adce70f0523b939fce0
Ronny Roeller <ronny@collaborne.com>
2021-02-17 11:12:43 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BoldExtension } from 'remirror/extensions';
+import { Remirror, useRemirror } from '@remirror/react';
 import './App.css';
 
 function App() {
+  const { manager, onChange, state } = useRemirror({
+    extensions: () => [new BoldExtension()],
+    content: '<p><strong>I am strong.</strong> and <em>I am emphasized</em></p>',
+    stringHandler: 'html',
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Remirror manager={manager} onChange={onChange} state={state} />
     </div>
   );
 }
